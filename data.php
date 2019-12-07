ab34b00fae3dca3634ea72c1fc69a211e109ca4b
Yamin2810 <51230292+Yamin2810@users.noreply.github.com>
2019-12-07 22:12:04 +0700

    Update data.php

--- data.php
+++ data.php
@@ -81,7 +81,7 @@
                                     </a>
                                 </li>
                                 <li class="nav-item">
-                                    <a href="./index3.html" class="nav-link active">
+                                    <a href="data.php" class="nav-link active">
                                         <i class="far fa-circle nav-icon"></i>
                                         <p>History Order</p>
                                     </a>
