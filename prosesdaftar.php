d74ef601e7a07a99351050b05f785eda8e288409
Yamin2810 <51230292+Yamin2810@users.noreply.github.com>
2019-12-07 19:37:46 +0700

    Update prosesdaftar.php

--- prosesdaftar.php
+++ prosesdaftar.php
@@ -1,23 +1,29 @@
 <?php
    require_once("database.php");
-   $username = $_POST['username'];
-   $pass = $_POST['password'];
-   $ = $_POST['p'];
-   $email = $_POST['email'];
-   $sql = "SELECT * FROM user WHERE username = '$username'";
-   $query = $connection->query($sql);
+   // Ambil data dri form 
+   $email                   = $_POST['email'];
+   $pass                    = $_POST['password'];
+   $fName                   = $_POST['first_name'];
+   $lName                   = $_POST['last_name'];
+   $birthday                = $_POST['birthday'];
+   $jk                      = $_POST['gender'];
+   $phone                   = $_POST['phone'];
+   $lokasi                   = $_POST['alamat'];
+   // END ambil data dri form
+   $sql                     = "SELECT * FROM user WHERE email = '$email'";
+   $query                   = $connection->query($sql);
    if($query->num_rows != 0) {
-     echo "<div align='center'>Username Sudah Terdaftar! <a href='daftar.php'>Back</a></div>";
+     echo "<div align       = 'center'>Username Sudah Terdaftar! <a href='prosesdaftar.php'>Back</a></div>";
    } else {
      if(!$username || !$pass) {
-       echo "<div align='center'>Masih ada data yang kosong! <a href='daftar.php'>Back</a>";
+       echo "<div align     = 'center'>Masih ada data yang kosong! <a href='register.html'>Back</a>";
      } else {
-       $data = "INSERT INTO user (username, password,prodi,email,level) VALUES ('$username', '$pass','$p','$email','pengguna')";
-       $simpan = $connection->query($data);
+       $data                = "INSERT INTO user (user_id, fname, lname, birth, email, pass, jenis_kelamin, phone, lokasi, tipe) VALUES ('', '$fName', '$lName', '$birthday','$email', '$pass', '$jk', '$phone','$lokasi','pengguna')";
+       $simpan              = $connection->query($data);
        if($simpan) {
-         echo "<div align='center'>Pendaftaran Sukses, Silahkan <a href='index.php'>Login</a></div>";
+         echo "<div align   = 'center'>Pendaftaran Sukses, Silahkan <a href='index.html'>Login</a></div>";
        } else {
-         echo "<div align='center'>Proses Gagal!</div>";
+         echo "<div align   = 'center'>Proses Gagal!</div>";
        }
      }
    }
