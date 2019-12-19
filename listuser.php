92c6b8512e5904c6d801da37793824bf04e991aa
Yamin2810 <51230292+Yamin2810@users.noreply.github.com>
2019-12-19 17:52:17 +0700

    Update listuser.php

--- listuser.php
+++ listuser.php
@@ -35,9 +35,9 @@ include  'database.php';
         <!-- Main Sidebar Container -->
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <!-- Brand Logo -->
-            <a href="lte/index3.html" class="brand-link">
-                <img src="img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-                <span class="brand-text font-weight-light">AdminLTE 3</span>
+            <a href="halaman_admin.php" class="brand-link">
+                <img src="img/trash1.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+                <span class="brand-text font-weight-light">Admin TRASH IT</span>
             </a>
 
             <!-- Sidebar -->
@@ -45,10 +45,10 @@ include  'database.php';
                 <!-- Sidebar user (optional) -->
                 <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                     <div class="image">
-                        <img src="img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+                        <img src="img/avatar04.png" class="img-circle elevation-2" alt="User Image">
                     </div>
                     <div class="info">
-                        <a href="#" class="d-block">List User yang terdaftar</a>
+                        <a href="#" class="d-block"> <?php echo $_SESSION['tipe']?></a>
                     </div>
                 </div>
 
@@ -57,7 +57,7 @@ include  'database.php';
                     <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
 
 
-                        <li class="nav-item has-treeview menu-open">
+                    <li class="nav-item has-treeview menu-open">
                             <a href="#" class="nav-link active">
                                 <i class="nav-icon fas fa-tachometer-alt"></i>
                                 <p>
@@ -67,21 +67,21 @@ include  'database.php';
                             </a>
                             <ul class="nav nav-treeview">
                                 <li class="nav-item">
-                                    <a href="halaman_pengguna.php" class="nav-link">
+                                    <a href="halaman_admin.php" class="nav-link">
                                         <i class="far fa-circle nav-icon"></i>
                                         <p>Home</p>
                                     </a>
                                 </li>
                                 <li class="nav-item">
-                                    <a href="./index.html" class="nav-link">
+                                    <a href="form_organisasi.php" class="nav-link">
                                         <i class="far fa-circle nav-icon"></i>
                                         <p>Add New Organization ID</p>
                                     </a>
                                 </li>
                                 <li class="nav-item">
-                                    <a href="form_order.php" class="nav-link">
+                                    <a href="listuser.php" class="nav-link active">
                                         <i class="far fa-circle nav-icon"></i>
-                                        <p>User List</p>
+                                        <p>User Registration List</p>
                                     </a>
                                 </li>
                             </ul>
@@ -104,7 +104,7 @@ include  'database.php';
                         </div>
                         <div class="col-sm-6">
                             <ol class="breadcrumb float-sm-right">
-                                <li class="breadcrumb-item"><a href="#">Home</a></li>
+                                <li class="breadcrumb-item"><a href="halaman_admin.php">Home</a></li>
                                 <li class="breadcrumb-item active">DataTables</li>
                             </ol>
                         </div>
@@ -126,17 +126,25 @@ include  'database.php';
                                 <th>No</th>
                                 <th>Nama Depan</th>
                                 <th>Nama Belakang</th>
+                                <th>Email</th>
                                 <th>Lokasi</th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <td>Trident</td>
-                                <td>Internet Explorer 4.0
-                                </td>
-                                <td>Win 95+</td>
-                                <td> 4</td>
-                            </tr>
+                                            <?php
+                        $sql = "SELECT * FROM user";
+                        $query = mysqli_query($connection, $sql);
+                        while($pickup = mysqli_fetch_array($query)){
+                        echo "<tr>";
+                        
+                        echo "<td>".$pickup['user_id']."</td>";
+                        echo "<td>".$pickup['fname']."</td>";
+                        echo "<td>".$pickup['lname']."</td>";
+                        echo "<td>".$pickup['email']."</td>";
+                        echo "<td>".$pickup['lokasi']."</td>";                   
+                        echo "</tr>";
+                        }   
+                        ?>
 
                             </tfoot>
                     </table>
